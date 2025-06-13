b689c0eae3c8597e5c880a37ede3d5148dae8cbc
WangZhenyu <wangzhenyu.9264@bytedance.com>
2025-06-14 00:04:23 +0800

    fix: 环境变量

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,30 +1,38 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // 让静态资源路径加上 /h5/
-  server: {
-    open: true,
-    host: true,
-    port: 3001,
-    proxy: {
-      "/api": {
-        target: "http://192.168.3.6:8000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        secure: false,
+export default defineConfig(({ mode }) => {
+  // const env = loadEnv(mode, process.cwd());
+  // const apiBase = env.VITE_API_BASE;
+
+  return {
+    define: {
+      'process.env': {},
+    },
+    plugins: [react()],
+    base: '/', 
+    server: {
+      open: true,
+      host: true,
+      port: 3001,
+      // proxy: {
+      //   '/api': {
+      //     target: apiBase,
+      //     changeOrigin: true,
+      //     rewrite: (path) => path.replace(/^\/api/, ''),
+      //     secure: false,
+      //   },
+      // },
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '~': '/src',
       },
     },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '~': '/src',
+    build: {
+      target: 'es2020',
     },
-  },
-  build: {
-    target: 'es2020', // 降级 JS 目标版本
-  },
+  };
 });
