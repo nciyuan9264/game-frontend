c9a956a2967c4a07a2fe90d21275aebb53d5347c
WangZhenyu <wangzhenyu.9264@bytedance.com>
2025-06-13 23:50:52 +0800

    fix: 环境变量

--- frontend/src/api/room.ts
+++ frontend/src/api/room.ts
@@ -1,3 +1,4 @@
+import { baseURL } from "@/const/env";
 import { Result } from "@/types/http";
 import { CreateRoomRequest, CreateRoomReponse, GetRoomListReponse } from "@/types/room";
 import axios from "axios";
@@ -5,9 +6,9 @@ import axios from "axios";
 
 
 export const createRoom = async (params: CreateRoomRequest): Promise<Result<CreateRoomReponse>> => {
-  return (await axios.post('/room/create', params)).data;
+  return (await axios.post(`http:${baseURL}:8000/api/room/create`, params)).data;
 };
 
 export const getRoomList = async (): Promise<Result<GetRoomListReponse>> => {
-  return (await axios.get('/room/list')).data;
+  return (await axios.get(`http:${baseURL}:8000/api/room/list`)).data;
 };
