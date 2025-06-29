f2990eda70fa16d4c5c3889d8ee9fbdf441e1e38
WangZhenyu <wangzhenyu.9264@bytedance.com>
2025-06-29 17:27:18 +0800

    fix: 音效

--- frontend/src/view/Splendor/Room/index.tsx
+++ frontend/src/view/Splendor/Room/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useWebSocket } from '@/hooks/useWebSocket';
 import styles from './index.module.less';
 import { useNavigate, useParams } from 'react-router-dom';
