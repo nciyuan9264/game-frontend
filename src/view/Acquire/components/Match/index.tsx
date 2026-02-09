04a8a59123d1b05cbcab198ff4783b8565755995
WangZhenyu <wangzhenyu.9264@bytedance.com>
2026-02-09 23:55:00 +0800

    feat: 房主修正

--- src/view/Acquire/components/Match/index.tsx
+++ src/view/Acquire/components/Match/index.tsx
@@ -43,7 +43,7 @@ export const Match: FC<IGameProps> = ({ sendMessage, wsRef, wsMatchSyncData, use
       seatList.push({
         id: `player-${index}`,
         label: player.playerID,
-        role: isHostView ? Role.Host : Role.Player,
+        role: player.playerID === wsMatchSyncData.room.ownerID ? Role.Host : Role.Player,
         isReady: player.ready,
       });
     });
