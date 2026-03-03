cdf9af0a55d15090d53b5589af98c79f527971c9
WangZhenyu <wangzhenyu.9264@bytedance.com>
2026-03-03 21:15:47 +0800

    feat: 弹窗ui优化

--- src/view/Acquire/components/Game/components/GameEnd/index.tsx
+++ src/view/Acquire/components/Game/components/GameEnd/index.tsx
@@ -74,7 +74,7 @@ const GameEnd: React.FC<GameEndProps> = ({
                   key={player}
                   initial={{ opacity: 0, y: 20 }}
                   animate={{ opacity: 1, y: 0 }}
-                  transition={{ delay: index * 0.1 }}
+                  transition={{ delay: index * 0.2 }}
                   className={styles.rankItem}
                   style={{ backgroundColor: bgColor }}
                 >
